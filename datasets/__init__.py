e80da114a64ee99a7e74e9f7df9301dd227c3f19
Xyz <xuyizhi1996@gmail.com>
2023-11-15 19:24:09 +0800

    functions to read annotations

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -4,9 +4,21 @@ Dataset package > __init__.py
 
 Containing functions to find, create, and forward data to training pipeline.
 """
+import os
+from xml.etree import ElementTree as ET
 
 def read_annotation(path) -> dict:
-    pass
+    tree = ET.parse(path)
+    return tree
 
-def get_bounding_boxes(annotation):
-    pass
+def get_bounding_boxes(xml_elementTree):
+    annotation = xml_elementTree
+    root = annotation.getroot()
+    labels = []
+    bounding_boxes = []
+    for child in root:
+        if child.tag == "object":
+            labels.append(child[0].text)
+            bounding_box = [int(element.text) for element in child[3]]
+            bounding_boxes.append(bounding_box)
+    return labels, bounding_boxes
